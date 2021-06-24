a4819d117c084a91bc1d4c9f0b60491f56a7e85b
tinyww <748826425@qq.com>
2021-06-24 17:35:10 +0800

    第一次提交

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+const registerRouter = require('./backend/router')
+module.exports = {
+  css: {
+    loaderOptions: {
+      sass: {
+        // 全局引入变量和 mixin
+        additionalData: `
+          @import "@/assets/scss/variable.scss";
+          @import "@/assets/scss/mixin.scss";
+        `
+      }
+    }
+  }
+}
